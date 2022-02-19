5d77a1f2f2510b62910f66937305e841dcfe05c6
15901505424 <15901505424@163.com>
2022-02-20 03:16:26 +0800

    alibaba cloud sample demo

---
+++ nacos-provider-sample-B/src/main/java/com/czscloud/czs/sample/B/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.czscloud.czs.sample.B.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/test")
+    public String test(){
+        return "hello, test :"+port;
+    }
+}
